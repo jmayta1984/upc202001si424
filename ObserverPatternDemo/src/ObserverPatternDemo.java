81743357af86ae395147460c13b539110c698efc
jmayta1984 <Monica$2017>
2020-04-22 08:09:13 -0500

    Observer Pattern Demo

---
+++ ObserverPatternDemo/src/ObserverPatternDemo.java
@@ -0,0 +1,13 @@
+public class ObserverPatternDemo {
+
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+
+        new BinaryObserver(subject);
+        new OctalObserver(subject);
+        new HexaObserver(subject);
+
+        System.out.println("First state change: 15");
+        subject.setState(15);
+    }
+}
