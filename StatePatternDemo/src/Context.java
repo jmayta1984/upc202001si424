3546ee81c16e66838891c1bb56ea2a5780c86810
jmayta1984 <Monica$2017>
2020-04-22 08:33:19 -0500

    Stater Pattern Design

---
+++ StatePatternDemo/src/Context.java
@@ -0,0 +1,15 @@
+public class Context {
+    public State state;
+
+    public Context() {
+        this.state = null;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+}
