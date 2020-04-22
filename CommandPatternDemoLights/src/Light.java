c8b708e01a60529c6e8384bf3567faf3e384cb7a
jmayta1984 <Monica$2017>
2020-04-22 06:41:49 -0500

    Command Pattern Design

---
+++ CommandPatternDemoLights/src/Light.java
@@ -0,0 +1,15 @@
+public class Light {
+
+    private boolean on;
+
+    public void swithOff() {
+        on = false;
+        System.out.println("Lights: " + on);
+    }
+
+    public void swithOn() {
+        on = true;
+        System.out.println("Lights: " + on);
+
+    }
+}
---
+++ CommandPatternDemoLights/src/LightOnCommand.java
@@ -0,0 +1,13 @@
+public class LightOnCommand implements Command{
+
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.swithOn();
+    }
+}
